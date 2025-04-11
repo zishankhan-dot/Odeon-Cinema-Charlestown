37f21912e3f3b91e40a2c87b80c15bfac2f85f7d
Zishan <kenzeesh@gmail.com>
2025-04-11 13:02:47 +0100

    updated frontend file for cart along with css file for cart

--- Frontend/js/carts.js
+++ Frontend/js/carts.js
@@ -3,32 +3,44 @@ document.addEventListener("DOMContentLoaded",()=>{
 })
 
 async function fetchMovies(){
-try{
-const response = await fetch("/cart/get",{
-    method:'get',
-})
-const movie= await response.json("cart");
-console.log(movie.cart);
+        const response = await fetch("/cart/get",{
+            method:'get',
+        }).then(async response=>{
+        if (response.status==200){
+            try{
+            const movie= await response.json("cart");
+            console.log(movie.cart);
 
 
-const container=document.getElementById("container");
-container.innerHTML="";
+            const container=document.getElementById("container");
+            container.innerHTML="";
 
 
-movie.cart.forEach(element => {
-    const card=document.createElement("div");
-    card.classList.add("card")
-    card.innerHTML=`
-    <img src="" alt="${element.MovieName}" class="card-img-top">
-    <div class="card-body">
-    <h2 class="card-header">MOVIE: ${element.MovieName}</h2>
-    <h3 class="card-header">QTY: ${element.qty}</h3>
-    </div>`;
-    container.appendChild(card);
-});
+            movie.cart.forEach(element => {
+                const card=document.createElement("tr");
+                card.innerHTML=`
+                    <td scope="row">${element.MovieName}</td>
+                    <td scope="row">${element.qty}</td>
+                    <td scope="row">${element.price}</td>
+                    <td>
+                    <div scope='row' class="w-50 btn-group h6 d-flex justify-content-middle w-100">
+                                <button type="button" class="btn btn-dark btn-sm plus" >+</button>
+                                <input type="text" style="width:20%;text-align: center;" class="input-number" value="${element.qty}">
+                                <button type="button" class="btn btn-dark minus" >-</button>
+                                <button type="button" class="btn btn-danger btn-update">UPDATE</button>
+                                <button type="button" class="btn btn-danger btn-update container-fluid">DELETE</button>
+                        </div>
+                    </td>`;
+                container.appendChild(card);
+            })}
+            catch(err){
+                console.error(err)
+            }
 
-}
-catch(err){
-    console.error(err)
-}
-}
+            }
+        else{  
+            const container=document.getElementById("table")
+            container.innerHTML=""
+            container.innerHTML=`<h1 class="fw-bolder"style="color:red">LOGIN TO SEE CART ITEMS !!</h1>`;
+        }})
+        }
